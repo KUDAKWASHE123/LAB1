3f4a1dd239ef66b9cba0487f88b829f9be997541
KUDAKWASHE123 <1493060@students.wits.ac.za>
2018-07-26 15:57:56 +0200

    Exercise 3.2

--- complex.cpp
+++ complex.cpp
@@ -8,8 +8,9 @@ using namespace std; // cout, endl, complex are all part of this namespace
 
 int main()
 {
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+    using Complex_number=complex<float>;
+	auto num1 = Complex_number{2.0, 2.0};  // use auto for type deduction
+	auto num2 = Complex_number{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
 
 	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
 
@@ -17,8 +18,8 @@ int main()
 	cout << "Or in a more familiar form: " << answer.real() 
 			<< " + " << answer.imag() << "j" 
 			<< endl	<< endl;
-//The post increment does not oparate on the complex data type because the post increment is of type int whereas
-// the complex number consists of two parts i.e real and imaginary part. So the program will be confused which part to increment 
+
 	// answer++;
+
 	return 0;
 }
