dbcfa327d7ebf46f8aeb987b186c1ba3c6fa93f3
KUDAKWASHE123 <1493060@students.wits.ac.za>
2018-07-26 16:29:53 +0200

    Exercise 4.1

--- scr_main.cpp
+++ scr_main.cpp
@@ -6,27 +6,27 @@ using namespace std;
 
 int main()
 {
-	auto myScreen = Screen{6,6};
-	myScreen.forward();
-	myScreen.set('*');
-	myScreen.down();
-	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
-
+	auto myScreen = Screen{6,6,' '};
+     
+for(int i=1;i<=6;i++)
+{
+myScreen.move(i,1);
+myScreen.set('*');
+}
+myScreen.move(1,6);
+myScreen.set('*');
+myScreen.move(6,6);
+myScreen.set('*');
+myScreen.move(2,4);
+myScreen.set('*');
+myScreen.move(3,2);
+myScreen.set('*');
+myScreen.move(5,4);
+myScreen.set('*');
+myScreen.move(4,2);
+myScreen.set('*');
+    
 	myScreen.display();
 	cout << endl;
-
-	myScreen.reSize(16,16);
-	myScreen.display();
-	myScreen.clear(' ');
-
-	myScreen.move(7,7);
-	myScreen.set("BIG");
-	myScreen.move(8,5);
-	myScreen.set("SCREEN");
-	myScreen.display();
-
 	return 0;
 }
-
