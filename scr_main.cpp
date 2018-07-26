2acf1908e396accdd05023e53f62c6ad6ac50e23
KUDAKWASHE123 <1493060@students.wits.ac.za>
2018-07-26 16:51:54 +0200

    Exercise 4.6

--- scr_main.cpp
+++ scr_main.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 int main()
 {
+//We can not modify  a class’s interface because most of the time they are close sources 
+//however we are able to perform imlementation because we might need to perform a function that is 
+//different from the given functions 
 	auto myScreen = Screen{10,10,'-'};
 myScreen.draw_square(5,5,5);
 	myScreen.display();
